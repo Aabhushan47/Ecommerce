080913ff29f3c690351de33b6fe8edd7c1f0502c
Aabhushan47 <khanalaabushan10@gmail.com>
2023-11-23 21:07:55 +0545

    Day 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { React } from 'react';
+import MyRoute from './MyRoute';
+
+
+function App() {
+  return (
+    <>
+    <MyRoute/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/First.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './First.css'
+
+const First = () => {
+  return (
+    <div className='demo'>First</div>
+  )
+}
+
+export default First
